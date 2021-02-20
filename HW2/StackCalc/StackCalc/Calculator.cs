1e1a5ae12b61896b0e256aa9704a10096f28fadb
pmokeev <pav3l.mokeev@gmail.com>
2021-02-20 22:18:42 +0300

    NOW add calculator + tests

---
+++ HW2/StackCalc/StackCalc/Calculator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace StackCalc
+{
+    class Calculator
+    {
+        private IStack stack;
+
+        public Calculator(IStack stack)
+        {
+            this.stack = stack;
+        }
+
+        private (float, float) GetTwoElementsFromStack()
+        {
+            float element1 = stack.Pop();
+            float element2 = stack.Pop();
+
+            return (element1, element2);
+        }
+
+        public double Calculate(string expressionToCalculate)
+        {
+            int numberToPush;
+            string[] expressionArray = expressionToCalculate.Split(" ");
+
+            foreach (var element in expressionArray)
+            {
+                if (Int32.TryParse(element, out numberToPush))
+                {
+                    stack.Push(numberToPush);
+                    continue;
+                }
+
+                switch (element)
+                {
+                    case "+":
+                        {
+                            var (element1, element2) = GetTwoElementsFromStack();
+                            stack.Push(element1 + element2);
+                            break;
+                        }
+                    case "-":
+                        {
+                            var (element1, element2) = GetTwoElementsFromStack();
+                            stack.Push(element2 - element1);
+                            break;
+                        }
+                    case "*":
+                        {
+                            var (element1, element2) = GetTwoElementsFromStack();
+                            stack.Push(element1 * element2);
+                            break;
+                        }
+                    case "/":
+                        {
+                            var (element1, element2) = GetTwoElementsFromStack();
+                            if (Math.Abs(element1) < 0.0000001)
+                            {
+                                throw new DivideByZeroException();
+                            }
+
+                            stack.Push(element2 / element1);
+                            break;
+                        }
+                    default:
+                        {
+                            throw new ArgumentOutOfRangeException();
+                        }
+                }
+            }
+
+            float answerExpression = stack.Pop();
+
+            if (!stack.IsEmpty())
+            {
+                throw new StackOverflowException();
+            }
+
+            return answerExpression;
+        }
+    }
+}
---
+++ HW2/StackCalc/StackCalc/TestsCalc.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace StackCalc
+{
+    class TestsCalc
+    {
+        private static bool ListStackCorrectTestCase()
+        {
+            var calculatorStack = new Calculator(new ListStack());
+            var expression = "1 2 3 + *";
+
+            return 5 == calculatorStack.Calculate(expression);
+        }
+
+        private static bool ListStackIncorrectTestCase()
+        {
+            var calculatorStack = new Calculator(new ListStack());
+            var expression = "1 2 +";
+
+            return 4 == calculatorStack.Calculate(expression);
+        }
+
+        private static bool ListStackOneNumberTestCase()
+        {
+            var calculatorStack = new Calculator(new ListStack());
+            var expression = "1";
+
+            return 1 == calculatorStack.Calculate(expression);
+        }
+
+        private static bool ListStackHardTestCase()
+        {
+            var calculatorStack = new Calculator(new ListStack());
+            var expression = "100 54 23 * + 10 2 / - 11 +";
+
+            return 1348 == calculatorStack.Calculate(expression);
+        }
+
+        private static bool ListStackMistakeOfThePastTestCase()
+        {
+            var calculatorStack = new Calculator(new ListStack());
+            var expression = "5 0 -";
+
+            return 5 == calculatorStack.Calculate(expression);
+        }
+
+        public static bool ListAllTestsCase()
+            => ListStackCorrectTestCase() && !ListStackIncorrectTestCase() && ListStackOneNumberTestCase() && ListStackHardTestCase() && ListStackMistakeOfThePastTestCase();
+
+        private static bool ArrayStackCorrectTestCase()
+        {
+            var calculatorStack = new Calculator(new ArrayStack());
+            var expression = "1 2 3 + *";
+
+            return 5 == calculatorStack.Calculate(expression);
+        }
+
+        private static bool ArrayStackIncorrectTestCase()
+        {
+            var calculatorStack = new Calculator(new ArrayStack());
+            var expression = "1 2 +";
+
+            return 4 == calculatorStack.Calculate(expression);
+        }
+
+        private static bool ArrayStackOneNumberTestCase()
+        {
+            var calculatorStack = new Calculator(new ArrayStack());
+            var expression = "1";
+
+            return 1 == calculatorStack.Calculate(expression);
+        }
+
+        private static bool ArrayStackHardTestCase()
+        {
+            var calculatorStack = new Calculator(new ArrayStack());
+            var expression = "100 54 23 * + 10 2 / - 11 +";
+
+            return 1348 == calculatorStack.Calculate(expression);
+        }
+
+        private static bool ArrayStackMistakeOfThePastTestCase()
+        {
+            var calculatorStack = new Calculator(new ArrayStack());
+            var expression = "5 0 -";
+
+            return 5 == calculatorStack.Calculate(expression);
+        }
+
+        public static bool ArrayAllTestsCase()
+            => ArrayStackCorrectTestCase() && !ArrayStackIncorrectTestCase() && ArrayStackOneNumberTestCase() && ArrayStackHardTestCase() && ArrayStackMistakeOfThePastTestCase();
+
+    }
+}
