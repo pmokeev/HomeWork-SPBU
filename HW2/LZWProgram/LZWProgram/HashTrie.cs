179b39d6360f6528bcfab2211aaa72968f11a241
pmokeev <pav3l.mokeev@gmail.com>
2021-02-23 12:18:37 +0300

    Add HW2.2

---
+++ HW2/LZWProgram/LZWProgram/HashTrie.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+
+namespace LZWProgram
+{
+    class HashTrie
+    {
+        private class HashTrieNode
+        {
+            public bool endOfWord;
+            public Hashtable hashArray;
+
+            public HashTrieNode()
+            {
+                this.endOfWord = false;
+                this.hashArray = new Hashtable();
+            }
+        }
+
+        private HashTrieNode root = new HashTrieNode();
+
+        public void Insert(string currentString)
+        {
+            if (currentString.Length == 0)
+            {
+                return;
+            }
+
+            HashTrieNode currentNode = root;
+
+            for (int i = 0; i < currentString.Length; i++)
+            {
+                var node = (HashTrieNode)currentNode.hashArray[currentString[i]];
+
+                if (node == null)
+                {
+                    node = new HashTrieNode();
+                    currentNode.hashArray.Add(currentString[i], node);
+                }
+
+                currentNode = node;
+            }
+
+            currentNode.endOfWord = true;
+        }
+
+        public bool IsExist(string currentString)
+        {
+            if (root == null || currentString.Length == 0)
+            {
+                return false;
+            }
+
+            HashTrieNode currentNode = root;
+
+            for (int i = 0; i < currentString.Length; i++)
+            {
+                var node = (HashTrieNode)currentNode.hashArray[currentString[i]];
+
+                if (node == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    currentNode = node;
+                }
+            }
+
+            return currentNode.endOfWord == true;
+        }
+    }
+}
---
+++ HW2/LZWProgram/LZWProgram/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LZWProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!TestsLZW.AllTestsTrie())
+            {
+                Console.WriteLine("Error!");
+                return;
+            }
+        }
+    }
+}
