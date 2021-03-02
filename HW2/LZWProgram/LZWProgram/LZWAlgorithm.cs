83a14d40b14d9d540e336b9239be4206eabac6e6
pmokeev <pav3l.mokeev@gmail.com>
2021-03-02 19:42:12 +0300

    Add beta-decompress

--- HW2/LZWProgram/LZWProgram/LZWAlgorithm.cs
+++ HW2/LZWProgram/LZWProgram/LZWAlgorithm.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Collections;
-using System.Collections.Generic;
 
 namespace LZWProgram
 {
@@ -65,6 +64,85 @@ namespace LZWProgram
 
                 currentByte = (byte)fileIn.ReadByte();
                 counterBytes++;
+            }
+        }
+
+        private static Hashtable FillHashtable()
+        {
+            var table = new Hashtable();
+
+            for (int i = 0; i < 256; i++)
+            {
+                table.Add(i, new byte[] { (byte)i });
+            }
+
+            return table;
+        }
+
+        private static void WriteInOutFile(byte[] byteArray, FileStream fileOut)
+        {
+            foreach (var item in byteArray)
+            {
+                fileOut.WriteByte(item);
+            }
+        }
+
+        private static int ReadKey(FileStream fileIn)
+        {
+            var arrayBytes = new byte[4];
+
+            for (int i = 0; i < arrayBytes.Length; i++)
+            {
+                arrayBytes[i] = (byte)fileIn.ReadByte();
+            }
+
+            Array.Reverse(arrayBytes);
+            return BitConverter.ToInt32(arrayBytes);
+        }
+
+        public static void Decompress(string pathToFile)
+        {
+            string resultPath = pathToFile.Substring(0, pathToFile.Length - 7);
+            using FileStream fileIn = File.OpenRead(pathToFile);
+            using FileStream fileOut = File.OpenWrite(resultPath);
+
+            Hashtable table = FillHashtable();
+
+            int countableIndex = 256;
+            int period = 4;
+            byte firstByte = 0;
+            byte[] bytesArray;
+
+            int oldKey = ReadKey(fileIn);
+            WriteInOutFile((byte[])table[oldKey], fileOut);
+
+            for (int i = 4; i < fileIn.Length; i += period)
+            {
+                int newKey = ReadKey(fileIn);
+
+                if (table.ContainsKey(newKey))
+                {
+                    bytesArray = (byte[])table[newKey];
+                }
+                else
+                {
+                    bytesArray = (byte[])table[oldKey];
+                    Array.Resize(ref bytesArray, bytesArray.Length + 1);
+                    bytesArray[bytesArray.Length - 1] = bytesArray[0];
+                }
+
+                WriteInOutFile(bytesArray, fileOut);
+
+                firstByte = bytesArray[0];
+
+                byte[] oldByteArray = (byte[])table[oldKey];
+                Array.Resize(ref oldByteArray, oldByteArray.Length + 1);
+                oldByteArray[oldByteArray.Length - 1] = firstByte;
+
+                countableIndex++;
+                table.Add(countableIndex, oldByteArray);
+
+                oldKey = newKey;
             }
         }
     }
