4dc04380963ae705726c5f581b85ae81844c5c70
pmokeev <pav3l.mokeev@gmail.com>
2021-03-15 18:00:09 +0300

    Add HW5.1

---
+++ HW5/StackCalc/StackCalc/ListStack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace StackCalc
+{
+    /// <summary>
+    /// A class that implements a stack on an list
+    /// </summary>
+    public class ListStack : IStack
+    {
+        private class Node
+        {
+            public float element;
+            public Node nextNode;
+
+            public Node(float element, Node nextNode)
+            {
+                this.element = element;
+                this.nextNode = nextNode;
+            }
+        }
+
+        private Node head;
+
+        /// <summary>
+        /// Push element in stack
+        /// </summary>
+        public void Push(float element)
+            => head = new Node(element, head);
+
+        /// <summary>
+        /// Pop element from stack
+        /// </summary>
+        public float Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            var popElement = head.element;
+            head = head.nextNode;
+            return popElement;
+        }
+
+        /// <summary>
+        /// Checking the stack for emptiness
+        /// </summary>
+        public bool IsEmpty()
+            => head == null;
+    }
+}
---
+++ HW5/StackCalc/StackCalc/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace StackCalc
+{
+    class Program
+    {
+        private static void PrintHello()
+        {
+            Console.WriteLine("Choose one stack option");
+            Console.WriteLine("1 - List stack");
+            Console.WriteLine("2 - Array stack");
+        }
+
+        private static int EntryKey()
+        {
+            while (true)
+            {
+                if (!Int32.TryParse(Console.ReadLine(), out int key) || (key != 1 && key != 2))
+                {
+                    Console.WriteLine("Try again!");
+                }
+                else
+                {
+                    return key;
+                }
+            }
+        }
+
+        private static string EnterExpression()
+        {
+            string expression = Console.ReadLine();
+            while (expression.Length == 0 || expression == "\n")
+            {
+                Console.WriteLine("Try again!");
+                expression = Console.ReadLine();
+            }
+
+            return expression;
+        }
+
+        static void Main(string[] args)
+        {
+            PrintHello();
+            int key = EntryKey();
+            Console.WriteLine("Enter the string to calculate:");
+            string expression = EnterExpression();
+
+            var calculatorStack = new Calculator(key == 1 ? new ListStack() : new ArrayStack());
+
+            try
+            {
+                Console.WriteLine($"Answer is: {calculatorStack.Calculate(expression)}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Error. Division by zero!");
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("Operations are less than numbers");
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("The expression must be of the form a b + . Not a + b");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Unknown character");
+            }
+        }
+    }
+}
