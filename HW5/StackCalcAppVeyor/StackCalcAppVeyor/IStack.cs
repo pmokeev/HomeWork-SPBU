44d3ee1a817996529f2788eb574950020ab126d7
pmokeev <pav3l.mokeev@gmail.com>
2021-03-15 20:09:20 +0300

    Add HW5.1

---
+++ HW5/StackCalcAppVeyor/StackCalcAppVeyor/IStack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StackCalcAppVeyor
+{
+    /// <summary>
+    /// Interface describing basic stack commands
+    /// </summary>
+    public interface IStack
+    {
+        void Push(float element);
+
+        float Pop();
+
+        bool IsEmpty();
+    }
+}
