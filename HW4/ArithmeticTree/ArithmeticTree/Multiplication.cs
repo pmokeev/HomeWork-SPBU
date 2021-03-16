99e55064abfd0736ff40b30e23cef0f9b94f936b
pmokeev <pav3l.mokeev@gmail.com>
2021-03-16 16:07:35 +0300

    Add HW4.1

---
+++ HW4/ArithmeticTree/ArithmeticTree/Multiplication.cs
@@ -0,0 +1,19 @@
+﻿namespace ArithmeticTree
+{
+    /// <summary>
+    /// Multiplication operation class
+    /// </summary>
+    public class Multiplication : Operator
+    {
+        /// <summary>
+        /// Operation sign
+        /// </summary>
+        public override char Operation { get; } = '*';
+
+        /// <summary>
+        /// Multiplication result
+        /// </summary>
+        public override float Calculate()
+            => LeftChild.Calculate() * RightChild.Calculate();
+    }
+}
---
+++ HW4/ArithmeticTree/ArithmeticTree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ArithmeticTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree();
+
+            Console.WriteLine("Enter expression:");
+            var expression = Console.ReadLine();
+
+            try
+            {
+                tree.CreateTree(expression);
+            }
+            catch (InvalidExpressionException)
+            {
+                Console.WriteLine("Error! Invalid expression!");
+                return;
+            }
+
+            Console.Write("Expression = ");
+            tree.PrintTree();
+            Console.WriteLine($"\nResult = {tree.Calculate()}");
+        }
+    }
+}
